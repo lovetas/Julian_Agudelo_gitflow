9dca6619846c71c927f735c6c31d6b60435de797
JulianAgudelo <julianagudelo19@hotmail.com>
2026-03-02 00:31:46 -0500

    feat: :sparkles: se creo menu de operaciones basicas
    menu de operaciones
    
    feature menu-operaciones

--- calculadora.py
+++ calculadora.py
@@ -8,6 +8,36 @@ def multiplicar(a, b):
     return a * b
 
 def dividir(a, b):
-    if b == 0:
+       if b == 0:
         return "Error: división por cero"
-    return a / b
+       return a / b
+
+while True:
+    print("\nCalculadora básica")
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    print("5. Salir")
+
+    opcion = input("Elige una opción (1-5): ")
+
+    if opcion == "5":
+        print("Saliendo del programa...")
+        break   
+
+    if opcion in ["1", "2", "3", "4"]:
+        num1 = float(input("Ingresa el primer número: "))
+        num2 = float(input("Ingresa el segundo número: "))
+
+        if opcion == "1":
+            print("Resultado:", sumar(num1, num2))
+        elif opcion == "2":
+            print("Resultado:", restar(num1, num2))
+        elif opcion == "3":
+            print("Resultado:", multiplicar(num1, num2))
+        elif opcion == "4":
+            print("Resultado:", dividir(num1, num2))
+    else:
+        print("Opción inválida")
+
